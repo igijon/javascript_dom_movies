d0e7396e0e433d858cda28e361caa7ee74eb8b9e
Inma Gijón <igijoninf@gmail.com>
2024-09-18 19:26:34 +0200

    Creación y borrado de elementos. Eventos botones. Imágenes añadidas.

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,17 +5,48 @@
 const NMOVIES = 5
 const NELEMENTSPMOVIE = 3
 
-const btnMovieConfiguration = () => {
+const btnMovieConfiguration = (movieDeck) => {
     const btnMovie = document.querySelector('#btnMovie')
     const divPelicula = document.querySelector("#pelicula-caratula")
     btnMovie.addEventListener('click', () => {
         let movie = getElement(movieDeck)
         
+        //Borramos la película antigua
+        let oldMovie = document.querySelector("#pelicula-caratula img")
+        if(oldMovie != null)
+            divPelicula.removeChild(oldMovie)
+        
+        //Borramos los elementos que haya desplegados de la película antigua
+        const elementsDiv = document.querySelector('#elementos-pelicula')
+        const oldElements = document.querySelectorAll('#elementos-pelicula .elemento')
+        while(elementsDiv.firstChild) {
+            elementsDiv.removeChild(elementsDiv.firstChild)
+        }
+        //Creamos de nuevo el montón de elementos
+        elementDeck = getElementsDeck()
+
         //Creamos el elemento de la película
         const imgMovie = document.createElement('img');
         imgMovie.src = `assets/movies/${movie}.jpg`
         imgMovie.classList.add('elemento')
         divPelicula.append(imgMovie)
+    })
+}
+
+const btnElementConfiguration = (elementDeck) => {
+    const btnElement = document.querySelector('#btnElement')
+    const divGlobal = document.querySelector("#elementos-pelicula")
+    btnElement.addEventListener('click', () => {
+        let element = getElement(elementDeck)
+        //Creamos el elemento 
+        const divElement = document.createElement('div')
+        divElement.classList.add('elemento')
+
+        const imgElement = document.createElement('img')
+        imgElement.src = `assets/characters/${element}.jpg`
+        imgElement.classList.add('recurso')
+        divElement.append(imgElement)
+        divGlobal.appendChild(divElement)
     })
 }
 
@@ -33,7 +64,7 @@ const getMoviesDeck = () => {
 const getElementsDeck = () => {
     let elementDeck = []
     for(let i = 1; i <= NMOVIES; i++) {
-        for(let j = 1; j <= NELEMENTSPMOVIE; j++) {
+        for(let j = 0; j < NELEMENTSPMOVIE; j++) {
             elementDeck.push("0"+i+"C"+j)
         } 
     }
@@ -52,4 +83,5 @@ const getElement = (elementDeck) => {
 
 let movieDeck = getMoviesDeck()
 let elementDeck = getElementsDeck()
-btnMovieConfiguration()
+btnMovieConfiguration(movieDeck)
+btnElementConfiguration(elementDeck)
